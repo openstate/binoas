2037199db88a1fe24b84c139317df84d72791a9d
Breyten Ernsting <bje@dds.nl>
2018-07-11 16:25:02 +0200

    Add a python container.

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# import locale
+# import os
+# import logging
+# from logging.handlers import SMTPHandler, RotatingFileHandler
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+# app.config.from_object(Config)
