fdc09e5758256fd9a508de4d458d9f9cc2870a32
Breyten Ernsting <bje@dds.nl>
2018-08-09 15:09:39 +0200

    Move workers in the binoas module.

--- loader.py
+++ loader.py
@@ -1,13 +1,7 @@
+#!/usr/bin/env python
 import sys
-import logging
 
-from worker import Consumer, start_worker
-
-
-class Loader(Consumer):
-    def output(self, transformed_message):
-        logging.info('Should save to Elasticsearch now!')
-        logging.info(transformed_message)
+from binoas.workers import Loader, start_worker
 
 if __name__ == "__main__":
     start_worker(sys.argv, Loader)
