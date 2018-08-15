10c8c5356089db1bda8d1182a3c22ea8efc40dcf
Breyten Ernsting <bje@dds.nl>
2018-08-15 11:26:06 +0200

    Add error handling in app and accpe json data.

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,44 @@ import logging
 # from logging.handlers import SMTPHandler, RotatingFileHandler
 from config import Config
 from binoas.utils import load_config
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify
 
-app = Flask(__name__)
-app.config.from_object(Config)
-app.config['binoas'] = load_config()['binoas']
+
+class BinoasError(Exception):
+    """API error class.
+    :param msg: the message that should be returned to the API user.
+    :param status_code: the HTTP status code of the response
+    """
+
+    def __init__(self, msg, status_code):
+        self.msg = msg
+        self.status_code = status_code
+
+    def __str__(self):
+        return repr(self.msg)
+
+    @staticmethod
+    def serialize_error(e):
+        return jsonify(dict(status='error', error=e.msg)), e.status_code
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.config['binoas'] = load_config()['binoas']
+
+    app.errorhandler(BinoasError)(BinoasError.serialize_error)
+
+    def add_cors_headers(resp):
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        # See https://stackoverflow.com/questions/12630231/how-do-cors-and-access-control-allow-headers-work
+        resp.headers['Access-Control-Allow-Headers'] = 'origin, content-type, accept'
+        return resp
+
+    app.after_request(add_cors_headers)
+
+    return app
+
+app = create_app()
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,40 @@
 import json
+from functools import wraps
 
 from flask import render_template, request, redirect, url_for, flash, Markup, jsonify
-
 from kafka import KafkaConsumer, KafkaProducer
 
-from app import app
-# from app.models import User, UserQueries
+from app import app, BinoasError
+from binoas.subscriptions import Subscription
 
-#TODO: how about thread safety??
+
+def decode_json_post_data(fn):
+    """Decorator that parses POSTed JSON and attaches it to the request
+    object (:obj:`request.data`)."""
+
+    @wraps(fn)
+    def wrapped_function(*args, **kwargs):
+        if request.method == 'POST':
+            data = request.get_data(cache=False)
+            if not data:
+                raise BinoasError('No data was POSTed', 400)
+
+            try:
+                request_charset = request.mimetype_params.get('charset')
+                if request_charset is not None:
+                    data = json.loads(data, encoding=request_charset)
+                else:
+                    data = json.loads(data)
+            except:
+                raise BinoasError('Unable to parse POSTed JSON', 400)
+
+            request.data = data
+
+        return fn(*args, **kwargs)
+
+    return wrapped_function
+
+# TODO: how about thread safety??
 producer = KafkaProducer(
     bootstrap_servers=app.config['binoas']['zookeeper'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8'))
@@ -18,20 +45,35 @@ def index():
     return jsonify(app.config['binoas'])
 
 
-@app.route("/posts/new")
+@app.route("/posts/new", methods=['POST'])
+@decode_json_post_data
 def new_post():
-    producer.send('topic', {
-        'application': 'test',
-        'payload': {
-            'id': 1,
-            'description': 'Dit is een test',
-            'topics': [
-                {'id': 1, 'name': 'tag1'},
-                {'id': 1, 'name': 'tag2'},
-            ]
+    if len(request.data.keys()) == 0:
+        payload = {
+            'application': 'test',
+            'payload': {
+                'id': 1,
+                'description': 'Dit is een test',
+                'topics': [
+                    {'id': 1, 'name': 'tag1'},
+                    {'id': 1, 'name': 'tag2'},
+                ]
+            }
         }
+    else:
+        payload = request.data
+
+    producer.send('topic', payload)
+
+    return jsonify({
+        'status': 'ok'
     })
-    return u''
+
+
+@app.route("/subscriptions/new", methods=['POST'])
+@decode_json_post_data
+def new_subscription():
+    return jsonify(request.data)
 
 if __name__ == "__main__":
     app.run(threaded=True)
