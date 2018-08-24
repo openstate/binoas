388581363c0d7da5b94917a01de680c1c93b734a
Breyten Ernsting <bje@dds.nl>
2018-08-24 17:20:07 +0200

    Find user with their queries based on the documents we percolated.

--- binoas/digest.py
+++ binoas/digest.py
@@ -7,12 +7,15 @@ from elasticsearch.exceptions import NotFoundError
 from elasticsearch.helpers import scan
 
 from binoas.es import setup_elasticsearch
+from binoas.db import setup_db
+from binoas.models import User, UserQueries
 
 
 class Digest:
     def __init__(self, config):
         self.config = config
         self.es = setup_elasticsearch(self.config)
+        self.db = setup_db(self.config)
 
     def _make_percolate_query(self, index_name, r):
         return {
@@ -70,6 +73,50 @@ class Digest:
         except ValueError as e:
             results = {'responses': []}
 
+        queries = {}
         for d, r in zip(scan_results, results['responses']):
-            if r['hits']['total'] > 0:
-                logging.info((d, r))
+            if r['hits']['total'] <= 0:
+                continue
+            #logging.info('Document %s' % (d['_id'],))
+            for q in r['hits']['hits']:
+                #logging.info('* %s' % (q['_id'],))
+                try:
+                    queries[q['_id']]['documents'].append(d['_source'])
+                except LookupError:
+                    queries[q['_id']] = {
+                        'query': q['_source'],
+                        'documents': [d['_source']]
+                    }
+
+        user_queries = self.db.query(UserQueries).filter(
+            UserQueries.query_id.in_(queries.keys())
+        ).filter(
+            UserQueries.frequency == None
+        ).all()
+        logging.info('Found user queries:')
+        logging.info([u.user_id for u in user_queries])
+
+        users_with_queries = {}
+        for uq in user_queries:
+            try:
+                users_with_queries[uq.user_id].append(uq)
+            except LookupError:
+                users_with_queries[uq.user_id] = [uq]
+
+        for user_id, uq in users_with_queries.items():
+            logging.info(
+                '%s alert for %s with ids : %s and %s documents' % (
+                    application, uq[0].user.email,
+                    [u.query_id for u in uq],
+                    sum([len(queries[q]['documents']) for q in [u.query_id for u in uq]])))
+            # logging.info({
+            #     'application': application,
+            #     'payload': {
+            #         'alerts': [
+            #             queries[u.query_id] for u in users_with_queries[user_id]],
+            #         'user': {
+            #             'id': uq[0].user_id,
+            #             'email': uq[0].user.email
+            #         }
+            #     }
+            # })
