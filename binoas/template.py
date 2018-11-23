07e08feed57cb7cda4be5a2edbb6684a99c380c5
Breyten Ernsting <bje@dds.nl>
2018-11-23 17:13:25 +0100

    Add a template for a welcome email upon subscription.

--- binoas/template.py
+++ binoas/template.py
@@ -7,38 +7,39 @@ class Templater:
         self.config = config
         self.jinja_env = Environment(loader=FileSystemLoader('.'))
 
-    def compile(self, message):
+    def _render(self, message, default_template, app_template, vars={}):
         application = message['application']
         payload = message['payload']
 
         try:
-            templ = self.jinja_env.get_template(
-                'templates/applications/%s.html' % (application,))
+            templ = self.jinja_env.get_template(app_template % (application,))
         except TemplateNotFound:
-            templ = self.jinja_env.get_template(
-                'templates/default/index.html')
-        return templ.render({
+            templ = self.jinja_env.get_template(default_template)
+
+        ctx_vars = {
             'application': application,
             'app': self.config['binoas']['applications'][application],
             'payload': payload,
             'config': self.config
-        })
+        }
+        ctx_vars.update(vars)
+
+        return templ.render(ctx_vars)
+
+    def compile(self, message):
+        return self._render(
+            message, 'templates/default/index.html',
+            'templates/applications/%s.html')
+
+    def compile_welcome(self, message):
+        return self._render(
+            message, 'templates/default/welcome.html',
+            'templates/applications/%s.welcome.html')
 
     def get_subject(self, message):
-        application = message['application']
-        payload = message['payload']
-        queries = [a['query']['description'] for a in payload['alerts']]
+        queries = [
+            a['query']['description'] for a in message['payload']['alerts']]
 
-        try:
-            templ = self.jinja_env.get_template(
-                'templates/applications/%s.subject.html' % (application,))
-        except TemplateNotFound:
-            templ = self.jinja_env.get_template(
-                'templates/default/subject.html')
-        return templ.render({
-            'application': application,
-            'app': self.config['binoas']['applications'][application],
-            'payload': payload,
-            'queries': ', '.join(queries),
-            'config': self.config
-        })
+        return self._render(
+            message, 'templates/default/subject.html',
+            'templates/applications/%s.subject.html', {'queries': queries})
