774a1f093011766572bb929ba6ac39db3884c238
Breyten Ernsting <bje@dds.nl>
2019-01-23 11:36:38 +0100

    Do not show location twice in emails for poliflw.

--- templates/filters/poliflw.py
+++ templates/filters/poliflw.py
@@ -6,7 +6,21 @@ def first_for_key(doc, key):
     return result
 
 
+def party_and_location(doc):
+    party = first_for_key('parties')
+    location = first_for_key('location')
+
+    if location is not None:
+        if location in party:
+            return party
+        else:
+            return "%s %s" % (party, location,)
+    else:
+        return party
+
+
 def filter_functions():
     return {
         'poliflw_first_for_key': first_for_key,
+        'poliflw_party_and_location': party_and_location
     }
