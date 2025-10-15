bcb76adf1599d1cb47b1dc2d4e8cb985e26a2d05
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-10-15 16:31:33 +0200

    Removes unnecessary quotes

--- bin/backup-es.sh
+++ bin/backup-es.sh
@@ -2,4 +2,4 @@
   
 CONTAINER="binoas_elastic_1"
 NEW_SNAPSHOT="backup1_$(date +%Y%m%d%H%M)"
-sudo docker exec $CONTAINER curl -XPUT 'http://localhost:9200/_snapshot/local_fs_backups/$NEW_SNAPSHOT'
+sudo docker exec $CONTAINER curl -XPUT http://localhost:9200/_snapshot/local_fs_backups/$NEW_SNAPSHOT
